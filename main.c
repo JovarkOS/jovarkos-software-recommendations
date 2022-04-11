0cc1693f9b912252f64d7590c8408db207e9997b
Gregory Powers <powersgr33@gmail.com>
2022-04-10 21:57:49 -0400

    streamlined button creation

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <gtk/gtk.h>
 #include "main.h"
+static void hide_buttons()
+{
+	for(int i=0; i< CATEGORY_NUMBER; i++)
+	{
+		gtk_widget_hide(category_buttons[i]);
+	}
+}
+
+static void on_button_click()
+{
+	hide_buttons();
+}
+
 static void activate (GtkApplication *app, gpointer user_data)
 {
-	  GtkWidget *window;
-	  GtkWidget *utilities_category_button;
-	  GtkWidget *games_category_button;
-	  GtkWidget *applications_category_button;
-	  GtkWidget *box;
+	 
 
 	  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
 	 
@@ -14,15 +23,13 @@ static void activate (GtkApplication *app, gpointer user_data)
 	  window = gtk_application_window_new (app);
 	  gtk_window_set_title (GTK_WINDOW (window), "Window");
 	  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
-
-	  //set buttons
-	  utilities_category_button = gtk_button_new_with_label ("Utilities");
-	  games_category_button = gtk_button_new_with_label("Games");
-	  applications_category_button = gtk_button_new_with_label("Applications");
-	  //pack them into a box
-	  gtk_box_pack_start(GTK_BOX(box),utilities_category_button, 1, 1, BUTTON_PADDING);
-	  gtk_box_pack_start(GTK_BOX(box),games_category_button, 1, 1, BUTTON_PADDING);
-	  gtk_box_pack_start(GTK_BOX(box),applications_category_button, 1, 1, BUTTON_PADDING);
+	  for(int i = 0; i < CATEGORY_NUMBER; i++)
+	  {
+	  	category_buttons[i] = gtk_button_new_with_label(category_names[i]);
+	  	gtk_box_pack_start(GTK_BOX(box), category_buttons[i], 1, 1, BUTTON_PADDING);
+	  	g_signal_connect(category_buttons[i], "clicked", G_CALLBACK(on_button_click), NULL);
+	  }
+	  
 	  gtk_box_set_spacing(GTK_BOX(box), 2);
 	  //put the box into the window
 	  gtk_container_add (GTK_CONTAINER (window), box);
--- main.h
+++ main.h
@@ -1,1 +1,10 @@
 #define BUTTON_PADDING 3
+#define CATEGORY_NUMBER 3
+GtkWidget *window;
+GtkWidget *category_buttons[CATEGORY_NUMBER];
+const char* category_names[] = {"Utilities", "Games", "Applications"};
+GtkWidget *box;
+
+static void hide_buttons();
+static void on_button_click();
+static void activate(GtkApplication *app, gpointer user_data);
