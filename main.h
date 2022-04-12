b7bd35b63327fe14fa57ebbb6d46627b864c71d1
Lucas Burlingham <lucas@lucasburlingham.me>
2022-04-11 21:15:18 -0400

    Add categories from SOFTWARE.ini

--- main.h
+++ main.h
@@ -1,9 +1,21 @@
 #define BUTTON_PADDING 3
-#define CATEGORY_NUMBER 3
+
+// Define list of category names found in SOFTWARE.ini
+const char* category_names[] = {
+    "Browsers",
+    "Text Editors",
+    "Terminal Emulators",
+    "Media",
+    "Internet Messaging",
+    "Development Software"
+     };
+GtkWidget *category_container;
+
+// Define the number of categories (find the number of elements)
+#define CATEGORY_NUMBER sizeof(category_names) / sizeof(category_names[0])
+
 GtkWidget *window;
 GtkWidget *category_buttons[CATEGORY_NUMBER];
-const char* category_names[] = {"Utilities", "Games", "Applications"};
-GtkWidget *category_container;
 
 static void hide_buttons();
 static void on_button_click();
